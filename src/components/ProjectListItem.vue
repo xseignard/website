fdf9651afacc0fcd15733b07034470c5aa3216f2
Anaïs <anais.appereg@gmail.com>
2018-06-29 15:38:26 +0200

    design and hover animation

--- src/components/ProjectListItem.vue
+++ src/components/ProjectListItem.vue
@@ -1,6 +1,7 @@
 <template>
 	<router-link :to="`/project/${project.id}`" @click.native="test" id="projectItem">
 		<div class="itemID img" :style="`${getBgImage(project.featured_image)}`">
+			<div class="color" :style="`background-color: ${getProjectColor(project)}`"></div>
 			<div class="hover">
 				<svg width="100%" height="100%">
 					<rect x="0" y="0" width="0" height="100%" :style="`fill:${getProjectColor(project)}`"/>
@@ -59,27 +60,37 @@ export default {
 		width: 70vw;
 	}
 }
+.color {
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	bottom: 0;
+	opacity: 0.4;
+}
 .hover {
 	width: 85%;
 	height: 100%;
 	position: absolute;
+	overflow: hidden;
 }
-.hover:hover .hover--details {
+#projectItem:hover .hover--details {
 	opacity: 1;
+	transform:translateX(0);
 }
 rect {
 	transition: 0.3s ease-out;
 }
-.hover:hover rect {
+#projectItem:hover rect {
 	width: 100%;
 }
 .hover--details {
+	transform:translateX(-10%);
 	opacity: 0;
 	padding: 5%;
 	box-sizing: border-box;
 	position: relative;
 	z-index: 1;
-	transition: 0.25s 0.1s ease-out;
+	transition: 0.25s 0.15s ease-out;
 }
 .baseline {
 	font-family: 'Open Sans Light';
