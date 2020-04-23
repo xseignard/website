694a8b2de79c53dd0beda5161ffe1ce7f5520d71
xseignard <xavier.seignard@gmail.com>
2020-04-23 17:41:24 +0200

    Updaed particles

---
+++ src/canvas/Particle.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three'
+
+import keyboard0 from '../assets/images/particles/keyboard-0.png'
+import keyboard1 from '../assets/images/particles/keyboard-1.png'
+import keyboard2 from '../assets/images/particles/keyboard-2.png'
+
+import mcu0 from '../assets/images/particles/mcu-0.png'
+import mcu1 from '../assets/images/particles/mcu-1.png'
+import mcu2 from '../assets/images/particles/mcu-2.png'
+
+import solder0 from '../assets/images/particles/solder-0.png'
+import solder1 from '../assets/images/particles/solder-1.png'
+import solder2 from '../assets/images/particles/solder-2.png'
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+const images = [
+  keyboard0,
+  keyboard1,
+  keyboard2,
+  mcu0,
+  mcu1,
+  mcu2,
+  solder0,
+  solder1,
+  solder2,
+]
+const loader = new THREE.TextureLoader()
+const materials = images.map(image => {
+  const texture = loader.load(image)
+  texture.wrapS = THREE.RepeatWrapping
+  texture.wrapT = THREE.RepeatWrapping
+  return new THREE.MeshBasicMaterial({
+    map: texture,
+    transparent: true,
+    opacity: 0.9,
+    side: THREE.DoubleSide,
+  })
+})
+const plane = new THREE.PlaneGeometry(1 / 256, 1 / 256)
+
+export default class Particle {
+  constructor(scene) {
+    this.geometry = plane
+    const random = Math.random()
+    if (random > 0.6) {
+      this.material = materials[getRandomInt(0, 3)]
+    } else if (random > 0.3) {
+      this.material = materials[getRandomInt(3, 6)]
+    } else {
+      this.material = materials[getRandomInt(6, 8)]
+    }
+    this.mesh = new THREE.Mesh(this.geometry, this.material)
+    this.mesh.position.set(0, 0, 1.5)
+    this.percent = Math.random()
+    this.offset = new THREE.Vector3(
+      (Math.random() - 0.5) * 0.1,
+      (Math.random() - 0.5) * 0.1,
+      0
+    )
+    this.speed = Math.random() * 0.001 + 0.0001
+    this.rotate = new THREE.Vector3(
+      -Math.random() * 0.1 + 0.01,
+      0,
+      Math.random() * 0.01
+    )
+    scene.add(this.mesh)
+  }
+  update(curve) {
+    this.percent += this.speed
+
+    const newPos = curve.getPoint(1 - (this.percent % 1)).add(this.offset)
+    this.mesh.position.x = newPos.x
+    this.mesh.position.y = newPos.y
+    this.mesh.position.z = newPos.z
+    this.mesh.rotation.x += this.rotate.x
+    this.mesh.rotation.y += this.rotate.y
+    this.mesh.rotation.z += this.rotate.z
+  }
+}
--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -3,137 +3,33 @@
 </template>
 <script>
 import * as THREE from 'three'
-
-const vertexShader = `
-  precision mediump float;
-
-  attribute vec3 aPosition;
-  attribute vec3 aRotationAxis;
-  attribute float aRotationAngle;
-
-  varying vec2 vUv;
-  varying float vRandom;
-
-  uniform float uTime;
-
-  #define PI 3.1415926538
-
-  mat4 rotationMatrix(vec3 axis, float angle) {
-    axis = normalize(axis);
-    float s = sin(angle);
-    float c = cos(angle);
-    float oc = 1.0 - c;
-    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
-                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
-                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
-                0.0,                                0.0,                                0.0,                                1.0);
-  }
-
-  vec3 rotate(vec3 v, vec3 axis, float angle) {
-    mat4 m = rotationMatrix(axis, angle);
-    return (m * vec4(v, 1.0)).xyz;
-  }
-
-  vec3 getDistortion(float at) {
-    float t = uTime * 0.0;
-    float mag = 0.5;
-    vec3 freq = vec3(1.32, 0.98, 1.137) * 5.;
-
-    vec3 distortion = vec3(
-      sin(at * freq.x + t), 
-      -sin(at * freq.y + t) * 0.1, 
-      cos(-at * freq.z + t)
-    );
-
-    distortion *= mag;
-
-    return distortion;
-  }
-
-  void main() {
-    vUv = uv;
-    vRandom = aRotationAngle;
-
-    float separation = 10.;
-    vec3 pos = position + aPosition * separation;
-
-    float t = uTime * 0.5;
-
-    vec3 distortion = getDistortion(pos.x); // Get a curvy distortion along the Y vertice component
-    pos += distortion;
-
-    // Rotate each instance geometry
-    pos = rotate(pos, aRotationAxis, aRotationAngle * PI * 2.);
-
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
-  }
-`
-
-const fragmentShader = `
-  precision mediump float;
-
-  varying vec2 vUv;
-  varying float vRandom;
-
-  uniform float uTime;
-
-  // https://iquilezles.org/www/articles/palettes/palettes.htm
-  vec3 palette(in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d) {
-    return a + b * cos(6.28318 * (c * t + d));
-  }
-
-  float map(float value, float min1, float max1, float min2, float max2) {
-    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
-  }
-
-  void main() {
-    float rt = map(vRandom, 0., 1., 0.05, 0.2); // Map value to a shorter range to have different progress in each geometry
-    float t = uTime * rt;
-    float o = fract(t); // Get fractional of time (0.1, 0.2 ... 0.99) for each second
-    float length = map(vRandom, 0., 1., 0.01, 0.02); // Map value to a shorter range to have different progress lengths
-
-    if(
-      abs(vUv.x - o) > length && 
-      abs(vUv.x - o - 1.) > length && 
-      abs(vUv.x - o + 1.) > length
-    ) {
-      discard; // Comment this line to see the whole lines/ribbons
-    }
-
-    float freq = map(vRandom, 0., 1., 1., 5.);
-    vec3 iQolor = palette(
-      sin(vUv.x * freq + t),
-      vec3(0.5, 0.5, 0.5),	
-      vec3(0.5, 0.5, 0.5),	
-      vec3(1.0, 1.0, 1.0), 
-      vec3(0.00, 0.33, 0.67) 
-    );
-
-    gl_FragColor = vec4(iQolor, 1.);
-  }
-`
+import Particle from '../canvas/Particle'
 
 export default {
-  data: () => ({ lines: [] }),
   mounted() {
     this.initThree()
-
-    this.createLines()
-
+    this.createCurve()
+    this.addParticles()
     const draw = () => {
       this.raf = window.requestAnimationFrame(draw)
-      this.material.uniforms.uTime.value = this.clock.getElapsedTime()
+      for (let i = 0; i < this.particles.length; i++) {
+        this.particles[i].update(this.curve)
+        if (this.particles[i].burst && this.particles[i].percent > 1) {
+          this.particles.splice(i, 1)
+          i--
+        }
+      }
       this.renderer.render(this.scene, this.camera)
     }
     draw()
     window.addEventListener('resize', () => {
       this.initThree()
-      this.createLines()
+      this.createCurve()
+      this.addParticles()
     })
   },
   destroyed() {
     window.cancelAnimationFrame(this.raf)
-    window.removeEventListener('resize', this.initThree)
   },
   methods: {
     initThree() {
@@ -141,11 +37,7 @@ export default {
       this.W = canvas.clientWidth
       this.H = canvas.clientHeight
 
-      this.clock = new THREE.Clock()
-
-      this.scene = new THREE.Scene()
-      this.camera = new THREE.PerspectiveCamera(50, this.W / this.H, 1, 1000)
-      this.camera.position.set(0, 0, 10)
+      // renderer
       this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
       const devicePixelRatio = window.devicePixelRatio
         ? Math.min(1.6, window.devicePixelRatio)
@@ -155,68 +47,28 @@ export default {
       this.renderer.setClearColor(new THREE.Color('#24003b'))
       if (canvas.firstChild) canvas.firstChild.remove()
       canvas.appendChild(this.renderer.domElement)
+
+      this.camera = new THREE.PerspectiveCamera(8, this.W / this.H, 0.01, 100)
+      this.camera.rotation.y = Math.PI
+      this.camera.position.z = 0.35
+      this.scene = new THREE.Scene()
+      this.scene.fog = new THREE.Fog(0x000d25, 0.05, 1.6)
     },
-    createLines() {
-      // Base Geometry
-      const baseGeometry = new THREE.PlaneBufferGeometry(3, 0.1, 1000, 1)
-
-      // Material
-      this.material = new THREE.ShaderMaterial({
-        vertexShader,
-        fragmentShader,
-        uniforms: {
-          uTime: new THREE.Uniform(null),
-        },
-        side: THREE.DoubleSide,
-      })
-
-      // Instance Geometry
-      const instanceCount = 300
-      const instancedGeometry = new THREE.InstancedBufferGeometry().copy(
-        baseGeometry
-      )
-      instancedGeometry.maxInstancedCount = instanceCount
-
-      // Instance Buffer Attributes
-      const aPosition = []
-      const aRotationAngle = []
-      const aRotationAxis = []
-      for (let i = 0; i < instanceCount; i++) {
-        aPosition.push(
-          2 * (Math.random() - 0.5),
-          2 * (Math.random() - 0.5),
-          2 * (Math.random() - 0.5)
-        )
-
-        aRotationAxis.push(
-          2 * (Math.random() - 0.5),
-          2 * (Math.random() - 0.5),
-          2 * (Math.random() - 0.5)
-        )
-
-        aRotationAngle.push(Math.random())
+    createCurve() {
+      const points = []
+      for (let i = 0; i < 5; i += 1) {
+        console.log((Math.random() * 10).toFixed(0))
+        points.push(new THREE.Vector3(0, 0, 2.5 * (i / 4)))
       }
-
-      const aPositionFloat32 = new Float32Array(aPosition)
-      const aRotationAngleFloat32 = new Float32Array(aRotationAngle)
-      const aRotationAxisFloat32 = new Float32Array(aRotationAxis)
-
-      instancedGeometry.setAttribute(
-        'aPosition',
-        new THREE.InstancedBufferAttribute(aPositionFloat32, 3, false)
-      )
-      instancedGeometry.setAttribute(
-        'aRotationAxis',
-        new THREE.InstancedBufferAttribute(aRotationAxisFloat32, 3, false)
-      )
-      instancedGeometry.setAttribute(
-        'aRotationAngle',
-        new THREE.InstancedBufferAttribute(aRotationAngleFloat32, 1, false)
-      )
-
-      // Mesh
-      const mesh = new THREE.Mesh(instancedGeometry, this.material)
-      this.scene.add(mesh)
+      points[4].y = -0.1
+      this.curve = new THREE.CatmullRomCurve3(points)
+      this.curve.type = 'catmullrom'
+    },
+    addParticles() {
+      this.particles = []
+      for (let i = 0; i < 300; i++) {
+        this.particles.push(new Particle(this.scene))
+      }
     },
   },
 }
